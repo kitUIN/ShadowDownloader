8fb831b8292b4bc91ae2b2c15b591b0ca6c02936
Nacho <kulujun@gmail.com>
2024-02-11 16:51:06 +0800

    percentConverter

---
+++ ShadowDownloader.UI/Converter/PercentConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace ShadowDownloader.UI.Converter;
+
+public class PercentConverter: IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, 
+        CultureInfo culture)
+    {
+        if (value is double percent && targetType.IsAssignableTo(typeof(string)))
+        {
+            return percent + "%";
+        }
+        return new BindingNotification(new InvalidCastException(), 
+            BindingErrorType.Error);
+    }
+
+    public object ConvertBack(object? value, Type targetType, 
+        object? parameter, CultureInfo culture)
+    {
+        if (value is not string percentString || !targetType.IsAssignableTo(typeof(double)))
+            return new BindingNotification(new InvalidCastException(),
+                BindingErrorType.Error);
+        percentString = percentString[..^2];
+        return System.Convert.ToDouble(percentString);
+    }
+}
