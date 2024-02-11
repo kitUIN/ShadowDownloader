483298acc8790525b8631aef0ca5e802721bc0ec
Nacho <kulujun@gmail.com>
2024-02-11 20:41:03 +0800

    新增四个Converter

--- ShadowDownloader.UI/Converter/TaskPlayEnableConverter.cs
+++ ShadowDownloader.UI/Converter/TaskPlayEnableConverter.cs
@@ -1,30 +1,25 @@
 ﻿using System;
 using System.Globalization;
-using Avalonia;
 using Avalonia.Data;
 using Avalonia.Data.Converters;
-using Avalonia.Media;
 using ShadowDownloader.Enum;
 
 namespace ShadowDownloader.UI.Converter;
 
-public class ProgressColorConverter : IValueConverter
+public class TaskPlayEnableConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not DownloadStatus status)
             return new BindingNotification(new InvalidCastException(),
                 BindingErrorType.Error);
-        Application.Current!.Styles.TryGetResource("AccentButtonBackground", Application.Current!.ActualThemeVariant,
-            out var color);
         return status switch
         {
-            DownloadStatus.Error => new SolidColorBrush(Color.FromRgb(248, 49, 47)),
-            DownloadStatus.Completed => new SolidColorBrush(Color.FromRgb(0, 210, 106)),
-            DownloadStatus.Running => color,
-            _ => new SolidColorBrush(Color.FromRgb(219, 155, 52))
+            DownloadStatus.Completed => false,
+            _ => true,
         };
     }
+
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
--- ShadowDownloader.UI/Converter/TaskStatusConverter.cs
+++ ShadowDownloader.UI/Converter/TaskStatusConverter.cs
@@ -1,28 +1,26 @@
 ﻿using System;
 using System.Globalization;
-using Avalonia;
 using Avalonia.Data;
 using Avalonia.Data.Converters;
-using Avalonia.Media;
+using FluentAvalonia.FluentIcons;
 using ShadowDownloader.Enum;
 
 namespace ShadowDownloader.UI.Converter;
 
-public class ProgressColorConverter : IValueConverter
+public class TaskStatusConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not DownloadStatus status)
             return new BindingNotification(new InvalidCastException(),
                 BindingErrorType.Error);
-        Application.Current!.Styles.TryGetResource("AccentButtonBackground", Application.Current!.ActualThemeVariant,
-            out var color);
+
         return status switch
         {
-            DownloadStatus.Error => new SolidColorBrush(Color.FromRgb(248, 49, 47)),
-            DownloadStatus.Completed => new SolidColorBrush(Color.FromRgb(0, 210, 106)),
-            DownloadStatus.Running => color,
-            _ => new SolidColorBrush(Color.FromRgb(219, 155, 52))
+            DownloadStatus.Error => FluentIconSymbol.DismissCircle24Filled,
+            DownloadStatus.Completed => FluentIconSymbol.CheckmarkCircle24Filled,
+            DownloadStatus.Running => FluentIconSymbol.Info24Filled,
+            _ => FluentIconSymbol.ErrorCircle24Filled
         };
     }
 
